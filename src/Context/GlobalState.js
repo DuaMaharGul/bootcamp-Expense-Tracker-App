156b8d411ed1da55f9f70735ad01bcf90e20fa73
Dua MG <duamahargul@outlook.com>
2022-07-08 09:48:22 +0500

    Commit App

---
+++ src/Context/GlobalState.js
@@ -0,0 +1,39 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer'; 
+
+// initial state
+const initialState = {
+    transactions: [
+        // { id: 1, text: 'Phone', amount: -20 },
+        // { id: 2, text: 'Salary', amount: 300 },
+        // { id: 3, text: 'Eating Out', amount: -10 },
+        // { id: 4, text: 'Traveling', amount: 150 }
+     ]
+}
+
+// global context
+export const GlobalContext = createContext(initialState);
+
+//global provider
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    
+    //actions that make call to reducer
+    function deleteTransaction (id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction (transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    return (<GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction, addTransaction}}>
+        {children}
+    </GlobalContext.Provider>);
+}
